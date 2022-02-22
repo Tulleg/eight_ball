02d07644684ec6afb3eaba95b5f35c7e360838d5
Tobias Geistert <geisterttobias@gmail.com>
2022-02-22 13:56:36 +0100

    comments and else

--- Magic_Eight_Ball.py
+++ Magic_Eight_Ball.py
@@ -1,7 +1,10 @@
 # Magic eight Ball
 import random
-name = input("Whats your name?")
+# Call User Name
+name = input("Whats your name?")  
+# Get User Question
 question = input("Tell me your Yes/No question.")
+# possible answers
 answers = ["Yes - definitely.",
              "It is decidedly so.", 
              "Without a doubt.", 
@@ -11,8 +14,11 @@ answers = ["Yes - definitely.",
              "My sources say no.", 
              "Outlook not so good.", 
              "Very doubtful."]
+# build random number with .random.randint between 0 incl 9
 random_number = random.randint(0,9)
-print(random_number)
+# check if we have Username and question 
 if name and question:
     print(f'{name} asks: {answers[random_number]}')
+else:
+    print("No question no answer!")
 
